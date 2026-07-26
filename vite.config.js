0eab2b75171e046a8ff5f76e841d6ea6feb2649c
Kauan Peçanha <kauanpecanha.dev@gmail.com>
2026-07-26 17:38:58 -0300

    fix phone number var

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/sac-pecanha/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: "/sac-pecanha/",
+    define: {
+      "import.meta.env.WHATSAPP_PHONE": JSON.stringify(
+        env.WHATSAPP_PHONE || "",
+      ),
+    },
+  };
 });
